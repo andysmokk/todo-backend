8f92f253589f1769ba49f173ebabe8d681908dee
Andrii Konovalov <andy.smokk@gmail.com>
2024-12-21 16:05:54 +0100

    added esolvers

--- src/schema/resolvers.ts
+++ src/schema/resolvers.ts
@@ -1,4 +1,4 @@
-const tasks = [
+let tasks = [
   {
     id: "ID-1",
     title: "The Awakening",
@@ -19,6 +19,40 @@ const resolvers = {
   Query: {
     getAllTasks: () => tasks,
     getTaskById: (_, { id }) => tasks.find((task) => task.id === id),
+  },
+
+  Mutation: {
+    createTask: (_, { input }) => {
+      const newTask = { id: `ID-${tasks.length + 1}`, ...input };
+
+      tasks.push(newTask);
+
+      return newTask;
+    },
+
+    updateTask: (_, { id, input }) => {
+      const index = tasks.findIndex((task) => task.id === id);
+
+      if (index === -1) {
+        throw new Error("Task not found");
+      }
+
+      tasks[index] = { ...tasks[index], ...input };
+
+      return tasks[index];
+    },
+
+    deleteTask: (_, { id }) => {
+      const index = tasks.findIndex((task) => task.id === id);
+
+      if (index === -1) {
+        throw new Error("Task not found");
+      }
+
+      tasks.splice(index, 1);
+
+      return "Task deleted successfully";
+    },
   },
 };
 
